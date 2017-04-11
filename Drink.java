bfd64f1c9a6841620c1480c336a6b372cfa1a3d7
Tyler Beachnau <stolenunder@yahoo.com>
2017-04-11 19:14:39 -0400

    Update Drink.java
    Update to drink class

--- Drink.java
+++ Drink.java
@@ -1,81 +1,97 @@
+
 /**
  * @author Tyler Beachnau
- * @since 2/26/2017
- * @version 0.1
+ * @since 4/1/2017
+ * @version 0.2
+ * 
  * CPS 410 - Group 1 project - Health Keeper
  * Subprogram: Blood Alcohol Calculator program
  * 
- * The Drink class represents an alcoholic drink. It is used to store a drink
- * and retrieve it's alcoholic content.
+ * The BACCalculator class represents the calculation functions for the BAC program.
+ * The handler passes data and calls methods to this class.
  * 
- * Key feature:
- * 1. Return the alcohol content of the drink.
- * 2. ANY TIME a change is made, updates the alcohol content (changing size/percentage yields recalculation of alcohol content)
+ * Features:
+ * 1. Drink list, holding a list of ALL the alcoholic drinks the user has added
+ * 2. Addition and Removal of drinks (including clearing of drink list).
+ * 3. Calculation of BAC via all drinks in it's list.
  */
 
 package healthkeeper;
 
+import java.util.LinkedList;
 
-public class Drink {
 
-	private float size;
-	private float percentage;
-	private float alcoholContent;
-	
-	/**
-	 * Constructor
-	 * @param size Size of the drink
-	 * @param percentage Percentage of alcohol in the drink.
-	 */
-	public Drink(float size, float percentage){
-		this.size = size;
-		this.percentage = percentage;
+public class BACCalculator {
+
+	private LinkedList<Drink> drinks = new LinkedList<Drink>();
+	private float bac = 0.0f;
 		
-		setAlcoholContent(size, percentage);
+	/**
+	 * Add a new drink to the end of the drink list.
+	 * @param drink, an object of the Drink class, representing an alcoholic drink.
+	 */
+	public void addDrink(Drink drink){
+		drinks.add(drink);
 	}
 	
 	/**
-	 * set method for size
-	 * @param size (size of the drink in fluid ounces!)
+	 * Clear the drinks in the drink list.
 	 */
-	public void setSize(float size){
-		this.size = size;
-		setAlcoholContent(this.size, this.percentage);
-	}
-        
-        public float getSize() {
-            return size;
-        }
-	
-	/**
-	 * set method for percentage
-	 * @param percentage (percentage of alcohol for the drink, i.e. the proof / 2)
-	 */
-	public void setPercentage(float percentage){
-		this.percentage = percentage;
-		setAlcoholContent(this.size, this.percentage);
+	public void clearDrinks(){
+		drinks.clear();
 	}
 	
 	/**
-	 * This method will set the alcohol content of this drink given parameters.
-	 * @param size The size in fl oz of the drink
-	 * @param percentage The percentage of alcohol per volume (abv)
-	 * @return Return the alcohol content of the drink.
+	 * Remove drink based on index (1 = first drink, 2 = second drink, n = last drink) 
+	 * @param index
 	 */
-	public void setAlcoholContent(float size, float percentage){
-		alcoholContent = size * percentage;
+	public void removeDrink(int index){
+		drinks.remove(index);
+                
+                for(Drink d: drinks){
+                    System.out.println(d);
+                }
 	}
 	
 	/**
-	 * This method returns the alcohol content of this drink
-	 * @return Return the alcohol content of this drink
+	 * Return BAC based on the formula: %BAC = (A x 5.14 / W x r) - .015 x H
+	 * A = alcohol content sum
+	 * W = Weight of the user
+	 * r = sex constant (.66 for female, .73 for male)
+	 * H = hours elapsed, i.e. 1 hour means H = 1.0, 1 hour and fifteen minutes means H = 1.25, etc
+	 * 
+	 * @param weight The weight of the "user" given in grams.
+	 * @param sex The biological sex of the person (female = true, male = false)
+	 * @return The BAC of the user, based on sex, weight, and drinks consumed
 	 */
-	public float getAlcoholContent(){
-		return alcoholContent;
+	public float calculateBAC(float weight, boolean sex, float hoursElapsed){
+		
+		//begin with gender constant tied to male
+		double sexConstant = .73;
+		
+		//if female (true), change sex constant to appropriate value
+		if(sex){
+			sexConstant = .66;
+		}
+		
+		double bacSum = 0.0;
+		for(Drink d: drinks){
+			bacSum += d.getAlcoholContent();
+		}
+		
+		this.bac = (float)((bacSum * 5.14 / weight * sexConstant) - .015 * hoursElapsed);
+		
+		if(this.bac < 0.0)
+			this.bac = 0.0f;
+		
+		return this.bac;
 	}
-        
-        public String toString(){
-            return "Size: " + size + " percentage: " + percentage;
-        }
-
+	
+	/**
+	 * 
+	 * @return Return BAC stored in the BACCalculator
+	 */
+	public float getBAC(){
+		return bac;
+	}
 }
